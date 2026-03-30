24356499a7f554c597dc2ee9835bb379c48a4036
starxmaker <beneath.black.skies@gmail.com>
2026-03-29 21:37:43 -0300

    feat: deploy to github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import pkg from './package.json'
 export default defineConfig(async () => {
   const react = (await import('@vitejs/plugin-react')).default
   return {
+    base: '/SudokuPWA/',
     plugins: [react()],
     define: { __APP_VERSION__: JSON.stringify(pkg.version) },
     server: { port: 5173 }
